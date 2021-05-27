4e7f041248f2c35ed13bb0cffedd6ca3f0b0d18d
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-05-27 13:58:47 -0300

    Add QueriesExecution class

---
+++ src/main/java/lesson2/QueriesExecution.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+import java.util.List;
+
+public class QueriesExecution {
+    public static void main(String[] args) {
+        AlunoDAO alunoDAO = new AlunoDAO();
+
+        List<Aluno> alunos = alunoDAO.list();
+
+        alunos.stream().forEach(System.out::println);
+    }
+}
