6b35b7aa4aa44239e08c103227f4cb714fbf99ae
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-05-27 14:27:32 -0300

    Add getById and insert examples

--- src/main/java/lesson2/QueriesExecution.java
+++ src/main/java/lesson2/QueriesExecution.java
@@ -6,8 +6,16 @@ public class QueriesExecution {
     public static void main(String[] args) {
         AlunoDAO alunoDAO = new AlunoDAO();
 
+        System.out.println("\n1 - Consulta todos os alunos");
         List<Aluno> alunos = alunoDAO.list();
-
         alunos.stream().forEach(System.out::println);
+
+        System.out.println("\n2 - Consulta por id");
+        Aluno aluno0 = alunoDAO.getById(3);
+        System.out.println(aluno0);
+
+        System.out.println("\n3 - Inserção");
+        Aluno aluno1 = new Aluno("Gabriel", 90, "DF");
+        alunoDAO.create(aluno1);
     }
 }
