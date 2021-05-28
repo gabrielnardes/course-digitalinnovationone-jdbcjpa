7572b0fd0cb730840b6480fc33cef9475ac1beb4
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-05-27 21:37:21 -0300

    Add JPA

--- src/main/java/lesson2/QueriesExecution.java
+++ src/main/java/lesson2/QueriesExecution.java
@@ -1,23 +1,25 @@
 package lesson2;
 
+import java.util.List;
+
 public class QueriesExecution {
     public static void main(String[] args) {
         AlunoDAO alunoDAO = new AlunoDAO();
 
-//        System.out.println("\n1 - Consulta todos os alunos");
-//        List<Aluno> alunos = alunoDAO.list();
-//        alunos.stream().forEach(System.out::println);
-//
-//        System.out.println("\n2 - Consulta por id");
-//        Aluno aluno0 = alunoDAO.getById(3);
-//        System.out.println(aluno0);
-//
-//        System.out.println("\n3 - Inserção");
-//        Aluno aluno1 = new Aluno("Gabriel", 90, "DF");
-//        alunoDAO.create(aluno1);
-//
-//        System.out.println("\n4 - Remoção");
-//        alunoDAO.delete(5);
+        System.out.println("\n1 - Consulta todos os alunos");
+        List<Aluno> alunos = alunoDAO.list();
+        alunos.stream().forEach(System.out::println);
+
+        System.out.println("\n2 - Consulta por id");
+        Aluno aluno0 = alunoDAO.getById(3);
+        System.out.println(aluno0);
+
+        System.out.println("\n3 - Inserção");
+        Aluno aluno1 = new Aluno("Gabriel", 90, "DF");
+        alunoDAO.create(aluno1);
+
+        System.out.println("\n4 - Remoção");
+        alunoDAO.delete(5);
 
         System.out.println("\n5 - Atualização");
         Aluno alunoAtualizado = alunoDAO.getById(7);
---
+++ src/main/java/lesson3/Main1.java
@@ -0,0 +1,31 @@
+package lesson3;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main1 {
+    public static void main(String[] args) {
+        // 1 - Create Entity Manager with factory, pointing to persistence.xml
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("part1-DIO");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        // 2 - Create instances
+        Estado estadoParaAdicionar = new Estado("Rio de Janeiro", "RJ");
+        Aluno alunoParaAdicionar = new Aluno("Daniel", 29, estadoParaAdicionar);
+
+        // 3 - Begin transaction
+        entityManager.getTransaction().begin();
+
+        // 4 - Persist data
+        entityManager.persist(estadoParaAdicionar);
+        entityManager.persist(alunoParaAdicionar);
+
+        // 5 - Commit
+        entityManager.getTransaction().commit();
+
+        // 6 - Close Entity Manager and its factory
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}
